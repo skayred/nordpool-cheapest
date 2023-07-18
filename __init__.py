d8fcea0d3cdc8ba8fa5f2c415ce526b7f8ff66ca
Dmitry Savchenko <sk_@Dmitrys-MacBook-Pro.local>
2023-07-18 12:59:53 +0300

    debug

--- __init__.py
+++ __init__.py
@@ -5,8 +5,6 @@ import voluptuous as vol
 from homeassistant.const import CONF_NAME
 from homeassistant.helpers.entity import Entity
 
-from sensor import CheapestHoursTracker
-
 _LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "nordpool-cheapest"
@@ -30,53 +28,22 @@ CONFIG_SCHEMA = cv.schema_with_slug_keys(
 )
 
 async def async_setup(hass, config):
-    """Set up the Hourly Calendar Event component."""
     _LOGGER.error("Initial")
-    entities = []
+    """Set up the Hourly Calendar Event component."""
+    if DOMAIN not in config:
+        return True
+    
+    nordpool = config[DOMAIN].get("nordpool")
 
     _LOGGER.error("Nordpool reading")
+    _LOGGER.error(nordpool)
 
-    sensor_state = hass.states.get(cfg.get("nordpool"))
-    attributes = sensor_state.attributes
+    for entry_config in config[DOMAIN].get("events"):
+        title = entry_config.get("name", "Hourly Event")
+        start_hour = entry_config.get("start_hour")
+        end_hour = entry_config.get("end_hour")
+        length = entry_config.get("length")
 
-    hass.data.setdefault(DOMAIN, {})
-
-    for entity_id, cfg in config[DOMAIN].items():
-        name = cfg.get(CONF_NAME, entity_id)
-        events = cfg["events"]
-
-        entity = CheapestHoursTracker(entity_id, name, events, attributes)
-        entities.append(entity)
-
-        # Schedule the initial event creation
-        await entity.async_create_events()
-
-    return True
-
-async def async_setup_entry(hass, entry):
-    """Set up the Hourly Calendar Event entry."""
-    # Retrieve and store the configuration options from the entry
-    title = entry.data.get("title")
-    events = entry.data.get("events")
-
-    # Perform setup tasks based on the configuration options
-    # ...
-
-    # Store any information that needs to be accessed later
-    hass.data[DOMAIN][entry.entry_id] = {
-        "title": title,
-        "events": events,
-    }
-
-    return True
-
-async def async_unload_entry(hass, entry):
-    """Unload the Hourly Calendar Event entry."""
-    # Perform cleanup tasks for the entry
-    # ...
-
-    # Remove stored information
-    if entry.entry_id in hass.data[DOMAIN]:
-        del hass.data[DOMAIN][entry.entry_id]
+        _LOGGER.info("Configuring %s with title '%s' and events: %s, %s, %s", DOMAIN, title, start_hour, end_hour, length)
 
     return True
